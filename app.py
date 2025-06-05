5d60ddb4728f669ef4d091b46cb619b275186ba6
Jayetheus <jayastro10@gmail.com>
2025-06-05 15:57:09 +0200

    added requirements

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+print(os.environ.get('SQLALCHEMY_DATABASE_URI'))
 
 
 app.config['SECRET_KEY'] = 'witrent_secret_key_that_is_super_secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
 CORS(app=app, origins=['*'])
 
